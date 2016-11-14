1aaf0f9a401601ff1cb79e287500e3f9c3ba8b5d
Patryk Piotrowski <patryk.piotrowski19@gmail.com>
2016-11-14 23:34:56 +0100

    Dodano: - nowa struktura bazy danych /source/mysql/sklep_internetowy.sql

--- source/mysql/sklep_internetowy.sql
+++ source/mysql/sklep_internetowy.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Paź 2016, 16:01
+-- Czas generowania: 14 Lis 2016, 22:34
 -- Wersja serwera: 5.7.9
 -- Wersja PHP: 5.6.16
 
@@ -23,30 +23,113 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `activation`
+--
+
+DROP TABLE IF EXISTS `activation`;
+CREATE TABLE IF NOT EXISTS `activation` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `code` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `expration_time` int(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `ID` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `login_session`
+--
+
+DROP TABLE IF EXISTS `login_session`;
+CREATE TABLE IF NOT EXISTS `login_session` (
+  `ID` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `token` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `expiration_date` int(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `password_reset`
+--
+
+DROP TABLE IF EXISTS `password_reset`;
+CREATE TABLE IF NOT EXISTS `password_reset` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `USER_ID` int(10) NOT NULL,
+  `CODE` varchar(40) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `EXPIRATION_TIME` int(32) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `ID` int(30) NOT NULL AUTO_INCREMENT,
+  `subcategory_id` int(30) NOT NULL,
+  `product_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cost` float NOT NULL,
+  `image` blob NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `subcategory`
+--
+
+DROP TABLE IF EXISTS `subcategory`;
+CREATE TABLE IF NOT EXISTS `subcategory` (
+  `ID` int(20) NOT NULL AUTO_INCREMENT,
+  `category_id` int(20) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `LOGIN` varchar(30) NOT NULL,
-  `PASSWORD` varchar(40) NOT NULL,
-  `NAME` varchar(30) DEFAULT NULL,
-  `SURNAME` varchar(30) DEFAULT NULL,
+  `LOGIN` varchar(30) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `PASSWORD` varchar(40) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `NAME` varchar(30) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `SURNAME` varchar(30) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
   `DATE_OF_BIRTH` date DEFAULT NULL,
-  `ADDRESS_TAB` varchar(200) DEFAULT NULL,
-  `ADDRESS_TAB2` varchar(200) DEFAULT NULL,
-  `POSTAL_CODE` varchar(10) DEFAULT NULL,
-  `CITY` varchar(50) DEFAULT NULL,
+  `ADDRESS_TAB` varchar(200) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `ADDRESS_TAB2` varchar(200) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `POSTAL_CODE` varchar(10) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `CITY` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `registered` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `users`
---
-
-INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `NAME`, `SURNAME`, `DATE_OF_BIRTH`, `ADDRESS_TAB`, `ADDRESS_TAB2`, `POSTAL_CODE`, `CITY`) VALUES
-(21, 'januksai', 'fd460ace781e7dc3991f9c139802d61d', 'das', 'das', NULL, 'dsa', NULL, 'das', 'da');
+) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
